a151bf84553bc136027b2fb9e6f95bc6fa72f362
Luana Alexandre <129203783+lualexan0@users.noreply.github.com>
2023-10-23 22:32:15 -0300

    AULA 05: PowerUps

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,9 +1,10 @@
 import pygame
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
+from dino_runner.utils.text_utils import draw_message_component
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
-from dino_runner.utils.text_utils import draw_message_component
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
 
 
 class Game:
@@ -23,6 +24,7 @@ class Game:
 
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
+        self.power_up_manager = PowerUpManager()
 
     def execute(self):
         self.running = True
@@ -36,9 +38,10 @@ class Game:
     def run(self):
         # Game loop: events - update - draw
         self.playing = True
+        self.obstacle_manager.reset_obstacles()
+        self.power_up_manager.reset_power_ups()
         self.game_speed = 20
         self.score = 0
-        self.obstacle_manager.reset_obstacles()
         while self.playing:
             self.events()
             self.update()
@@ -54,6 +57,7 @@ class Game:
         self.player.update(user_input)
         self.obstacle_manager.update(self)
         self.update_score()
+        self.power_up_manager.update(self)
 
     def update_score(self):
         self.score += 1
@@ -67,6 +71,8 @@ class Game:
         self.draw_score()      
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
+        self.power_up_manager.draw(self.screen)
+        self.draw_power_up_time()
         pygame.display.update()
         pygame.display.flip()
 
@@ -80,12 +86,27 @@ class Game:
         self.x_pos_bg -= self.game_speed
 
     def draw_score(self):
-       draw_message_component(
+        draw_message_component(
             f"Score: {self.score}",
             self.screen,
             pos_x_center=1000,
             pos_y_center=50
-        )    
+        )
+
+    def draw_power_up_time(self):
+        if self.player.has_power_up:
+            time_to_show = round((self.player.power_up_time - pygame.time.get_ticks()) / 1000, 2)
+            if time_to_show >= 0:
+                draw_message_component(
+                    f"{self.player.type.capitalize()} enabled for {time_to_show} seconds",
+                    self.screen,
+                    font_size = 18,
+                    pos_x_center = 500,
+                    pos_y_center= 50
+                )  
+            else:
+                self.player.has_power_up = False
+                self.player.type = DEFAULT_TYPE
 
     def handle_events_on_menu(self):
         for event in pygame.event.get():
@@ -108,14 +129,13 @@ class Game:
                 f"Your Score: {self.score}",
                 self.screen,
                 pos_y_center=half_screen_height - 150
-            )          
+            )
             draw_message_component(
                 f"Death count: {self.death_count}",
                 self.screen,
                 pos_y_center=half_screen_height - 100
             )
-            
-            self.screen.blit(ICON, (half_screen_width - 40, half_screen_height - 40))   
+            self.screen.blit(ICON, (half_screen_width - 40, half_screen_height - 40))
 
         pygame.display.update()
 
